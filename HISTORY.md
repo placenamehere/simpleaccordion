3cad5d959128f62540e9f4a6c177893db76a7e5b
Chris Casciano <chris@placenamehere.com>
2014-05-19 12:40:53 -0400

    Added callback function, and duration as options

--- HISTORY.md
+++ HISTORY.md
@@ -1,4 +1,7 @@
 # History
 
+* v0.6.0 May 19, 2014
+  * Added callback function, and duration as options
+  * misc cleanup
 * v0.5.0 October 23, 2013
 	* Added framework / grunt.js infrastructure based on Yeoman jquery-boilerplate
--- src/jquery.simpleaccordion.js
+++ src/jquery.simpleaccordion.js
@@ -1,31 +1,18 @@
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, document, undefined ) {
-
-		// undefined is used here as the undefined global variable in ECMAScript 3 is
-		// mutable (ie. it can be changed by someone else). undefined isn't really being
-		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-		// can no longer be modified.
-
-		// window and document are passed through as local variable rather than global
-		// as this (slightly) quickens the resolution process and can be more efficiently
-		// minified (especially when both are regularly referenced in your plugin).
 
 		// Create the defaults once
 		var pluginName = "simpleaccordion",
 				defaults = {
           prefix: "sa",
           triggerEvent: "click",
-          easing: "swing"
+          easing: "swing",
+          duration: 800,
+          callback: null
         };
 
 		// The actual plugin constructor
 		function Plugin ( element, options ) {
 			this.element = element;
-			// jQuery has an extend method which merges the contents of two or
-			// more objects, storing the result in the first object. The first object
-			// is generally empty as we don't want to alter the default options for
-			// future instances of the plugin
 			this.options = $.extend( {}, defaults, options );
 			this._defaults = defaults;
 			this._name = pluginName;
@@ -35,24 +22,12 @@
 		Plugin.prototype = {
 				init: function () {
 						// Place initialization logic here
-						// You already have access to the DOM element and
-						// the options via the instance, e.g. this.element
-						// and this.options
-						// you can add more functions like the one below and
-						// call them like so: this.yourOtherFunction(this.element, this.options).
-
             var _this = this,
                 $el = $(_this.element),
                 $headers = $el.children("dt"),
                 $openheader,
                 activeClassName = _this.options.prefix + "-active",
-                openClassName = _this.options.prefix + "-open",
-                heightDataAttr = _this.options.prefix + "-original-height";
-
-            // STEP 0: save initial drawn height of each accordion content
-            $el.children("dd").map(function() {
-              $(this).data(heightDataAttr,$(this).height());
-            });
+                openClassName = _this.options.prefix + "-open";
 
             // STEP 1: determine open item (default to first)
             $openheader = $headers.filter("."+openClassName);
@@ -74,17 +49,24 @@
 
               e.preventDefault();
 
-
-              $last.removeClass(openClassName).next().slideUp({ duration: 800, easing: _this.options.easing });
-              $which.addClass(openClassName).next().slideDown({ duration: 800, easing: _this.options.easing });
+              if (!$which.is("."+openClassName)) {
+                $last.removeClass(openClassName).next().slideUp({
+                  duration: _this.options.duration,
+                  easing: _this.options.easing
+                });
+                $which.addClass(openClassName).next().slideDown({
+                  duration: _this.options.duration,
+                  easing: _this.options.easing,
+                  complete: function() {
+                    if (typeof _this.options.callback === "function") {
+                      _this.options.callback.call($(this).prev("dt"));
+                    }
+                  }
+                });
+              }
             });
 
             // STEP 4: History State?
-
-
-				// },
-				// open: function (e) {
-				// 		e.preventDefault();
 				}
 		};
 
